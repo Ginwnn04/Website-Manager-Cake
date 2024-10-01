96ab71cff4d5d436eceab0bb05bab2df699e1830
Ginwnn04 <quangdeeptry1911@gmail.com>
2024-10-01 17:05:33 +0700

    header

---
+++ script.js
@@ -0,0 +1,19 @@
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementsByClassName("nav-bar")[0];
+    var firstItem = document.querySelector('.nav-bar ul li:first-child a');
+    if (window.pageYOffset >= 75) {
+      navbar.classList.add("top");  
+ 
+      firstItem.classList.add("test");
+      firstItem.classList.add("test1");
+
+      
+
+    }
+    else {
+      navbar.classList.remove("top"); 
+      firstItem.classList.remove("test");
+      firstItem.classList.remove("test1");
+    }
+});
+  
