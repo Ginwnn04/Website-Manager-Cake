dc7cf47e3268201fc8666d94002279f2a8bfb481
Ginwnn04 <quangdeeptry1911@gmail.com>
2024-10-01 20:03:40 +0700

    aaa

--- script.js
+++ script.js
@@ -3,12 +3,8 @@ window.addEventListener('scroll', function() {
     var firstItem = document.querySelector('.nav-bar ul li:first-child a');
     if (window.pageYOffset >= 75) {
       navbar.classList.add("top");  
- 
       firstItem.classList.add("test");
       firstItem.classList.add("test1");
-
-      
-
     }
     else {
       navbar.classList.remove("top"); 
