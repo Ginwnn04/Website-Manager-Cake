2c03efb9ba2c838bf7473893bc67ae18ac838392
CôngVinh-12102005 <lecongvinh12102005@gmail.com>
2024-12-03 14:36:48 +0700

    Cập nhật 3/12

--- admin_script.js
+++ admin_script.js
@@ -1717,6 +1717,8 @@ const categoryOptionFilter = document.getElementById('productCategoryFilter');
 let categories= [];
 let listProduct =  [];
 let editProductIndex = null; 
+let filteredProducts=[];
+let isFiltering = false;
 const fileLabel = document.getElementById('fileNameLabel');
 function toggleAddProductForm() {
   formTitleP.textContent='Thêm sản phẩm';
@@ -1895,7 +1897,7 @@ function saveProduct(productData) {
     editProductIndex = null;
   } else {
     listProduct.unshift(productData);
-    showToast('succuss',"Sản phẩm đã được thêm!");
+    showToast('success',"Sản phẩm đã được thêm!");
   }
   localStorage.setItem('listProduct', JSON.stringify(listProduct));
   cancelProduct();
@@ -1925,7 +1927,13 @@ editButton.style.display = 'none';
 
 function editP(index) {
   editProductIndex = index;
-  const product = listProduct[index];
+  let product;
+  if(isFiltering === true){
+    product = filteredProducts[index];
+  }
+  if(isFiltering === false){
+    product = listProduct[index];
+  }
   formP.style.display = 'block';
   formTitleP.textContent = 'Xem sản phẩm'; 
   document.getElementById('productName').value = product.name;
@@ -1939,6 +1947,7 @@ function editP(index) {
     preview.style.display = 'block';
     removeImageBtn.disabled=true;
   } else {
+    removeImageBtn.disabled=true;
     preview.src=img.src;
     preview.style.display = 'block';
   }
@@ -2117,7 +2126,7 @@ function updatePaginationDisplay(listProduct) {
   const pageButtonsContainer = document.querySelector('.page-numbers');
   pageButtonsContainer.innerHTML = ''; // Xóa các nút phân trang cũ
 
-  // Luôn hiển thị nút cho trang đầu tiên
+  if(listProduct.length>0)
   createPaginationButton(1, pageButtonsContainer);
   // Hiển thị các nút ở giữa nếu cần
   if (currentPageP > 4) {
@@ -2192,8 +2201,7 @@ function sortProduct(attribute) {
   localStorage.setItem('listProduct', JSON.stringify(listProduct));
   displayProducts(listProduct);
 }
-let filteredProducts=[];
-let isFiltering = false;
+
 function navigateToPageforFilter(action,filteredProducts) {
   const totalPages = Math.ceil(filteredProducts.length / maxIndex);
   if (action === 'first') currentPageP = 1;
@@ -2222,7 +2230,8 @@ function updatePaginationDisplayforFilter(filteredProducts) {
   pageButtonsContainer.innerHTML = ''; // Xóa các nút phân trang cũ
 
   // Luôn hiển thị nút cho trang đầu tiên
-  createPaginationButtonforfilter(1, pageButtonsContainer);
+  if(filteredProducts.length>0)
+    createPaginationButtonforfilter(1, pageButtonsContainer);
   // Hiển thị các nút ở giữa nếu cần
   if (currentPageP > 4) {
       // Nếu cách xa trang đầu tiên, thêm dấu "..."
@@ -2257,7 +2266,7 @@ function gotoPage() {
   const pageNumber = parseInt(input.value, 10); // Chuyển giá trị input sang số nguyên
 
   // Xác định nguồn dữ liệu dựa trên trạng thái của `filteredProducts`
-  const dataSource = (isFiltering===true && filteredProducts.length > 0) ? filteredProducts : listProduct;
+  const dataSource = (isFiltering===true) ? filteredProducts : listProduct;
   const totalPages = Math.ceil(dataSource.length / maxIndex); // Tính tổng số trang từ nguồn
 
   // Kiểm tra và điều hướng trang
